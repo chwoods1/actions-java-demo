cdae3b4339387c277348115a12be08456edbd2c9
chwoods1 <132087868+chwoods1@users.noreply.github.com>
2026-02-12 10:04:52 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
-  public static oid main(String[] args) {
-    System.out.println("Hello from github actions!")
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
 }
